220361a3701f1cccb4350c17ddc345aafec99503
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2022-06-26 23:09:08 +0100

    Day4
    I didn't really do a lot today
    I just worked on the animation part of the project section
    I didn't even finish. Today was not that productive

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 let toggleStatus = false;
 let firstime = true;
-function toggle(){
+const toggle = () => {
     if(toggleStatus === false && firstime === true){
         document.querySelector(".navCollapseOff").classList.replace("navCollapseOff","navCollapseOn");
         document.querySelector('main .wrapper').style.display="none";
-        document.querySelector('.aboutMe').style.display="none";
+        document.querySelector('article').style.display="none";
         firstime = false;
         toggleStatus = true;
     }else if(toggleStatus === true && firstime === false){
         document.querySelector(".navCollapseOn").classList.replace("navCollapseOn","navCollapseSlideOff");
         document.querySelector('main .wrapper').style.display="flex";
-        document.querySelector('.aboutMe').style.display="block";
+        document.querySelector('article').style.display="block";
         firstime = false;
         toggleStatus = false;
     }else if(toggleStatus === false && firstime === false){
         document.querySelector(".navCollapseSlideOff").classList.replace("navCollapseSlideOff","navCollapseOn");
         document.querySelector('main .wrapper').style.display="none";
-        document.querySelector('.aboutMe').style.display="none";
+        document.querySelector('article').style.display="none";
         firstime = false;
         toggleStatus = true;
     }
